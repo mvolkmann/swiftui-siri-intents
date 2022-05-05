c578ef1da4c479f1383e415114d8fadc5d357be9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-05 04:40:00 -0500

    Initial Commit

---
+++ swiftui-siri-intents/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-siri-intents
+//
+//  Created by Mark Volkmann on 5/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-siri-intents/swiftui_siri_intentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_siri_intentsApp.swift
+//  swiftui-siri-intents
+//
+//  Created by Mark Volkmann on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_siri_intentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
